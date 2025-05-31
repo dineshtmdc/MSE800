f90869abbcbe48f2a1a729ad81c84a3e94952879
Dinesh Tennakoon <dineshtmdc@gmail.com>
2025-06-01 11:29:42 +1200

    update actitvity 05

---
+++ WEEK08/Activity05.py
@@ -0,0 +1,26 @@
+# Parent class
+class Animal:
+    def speak(self):
+        print("The animal makes a sound.")
+
+# Child class 1
+class Dog(Animal):
+    def bark(self):
+        print("The dog barks.")
+
+# Child class 2
+class Cat(Animal):
+    def meow(self):
+        print("The cat meows.")
+
+# Create an instance of Dog
+my_dog = Dog()
+print("Dog says:")
+my_dog.speak()   # Inherited from Animal
+my_dog.bark()    # Defined in Dog
+
+print("\nCat says:")
+# Create an instance of Cat
+my_cat = Cat()
+my_cat.speak()   # Inherited from Animal
+my_cat.meow()    # Defined in Cat
