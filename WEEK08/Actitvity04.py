b6d9c7ec60e5e47ce177c2e9f8a24132ab178305
Dinesh Tennakoon <dineshtmdc@gmail.com>
2025-06-01 11:18:14 +1200

    upadte with example

--- WEEK08/Actitvity04.py
+++ WEEK08/Actitvity04.py
@@ -1,18 +1,18 @@
 # Parent class
-class Animal:
-    def speak(self):
-        print("The animal makes a sound.")
+class Device:
+    def power_on(self):
+        print("Device is now powered on.")
 
-# Child class inheriting from Animal
-class Dog(Animal):
-    def bark(self):
-        print("The dog barks.")
+# Child class inheriting from Device
+class Smartphone(Device):
+    def launch_app(self, app_name):
+        print(f"Launching {app_name} on smartphone...")
 
-# Creating an instance of Dog
-my_dog = Dog()
+# Creating an instance of Smartphone
+my_phone = Smartphone()
 
 # Calling method from parent class
-my_dog.speak()
+my_phone.power_on()
 
 # Calling method from child class
-my_dog.bark()
+my_phone.launch_app("Camera")
